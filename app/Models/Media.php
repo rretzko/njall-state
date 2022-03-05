e226bfead32d93a710cbb783329a163b849e036f
MFRHoldings <rick@mfrholdings.com>
2022-03-05 08:47:58 -0500

    Add Media + Mediatype models

---
+++ app/Models/Media.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['composition_id', 'event_id','link', 'mediatype_id',];
+}
