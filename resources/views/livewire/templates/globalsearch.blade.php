ae6b4417c2b84eb19a428369895d8314a5968d00
MFRHoldings <rick@mfrholdings.com>
2022-04-08 11:16:31 -0400

    Fix globalsearch.blade.php implementation.

--- resources/views/livewire/templates/globalsearch.blade.php
+++ resources/views/livewire/templates/globalsearch.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="input-group">
+    <div class="input-group flex flex-col">
         <label for="globalsearch" class="font-bold">Search by Year, Last Name or<br /> Word in Title</label>
         <input wire:model.debounce.500ms="globalsearch" class="rounded-full" type="text" id="globalsearch"
                placeholder="Enter search value"/>
