8b19a28cbd45e6127b6d10a2c40306f1470800d7
MFR Holdings <(rick@mfrholdings.com)>
2022-06-25 10:45:33 -0400

    Update mapping in FindSchoolTrait.php.

--- app/Traits/FindSchoolTrait.php
+++ app/Traits/FindSchoolTrait.php
@@ -73,26 +73,37 @@ trait FindSchoolTrait
     private function buildMap()
     {
         return [
+            'Audubon JR/SR HS' => 19,
             'Bergen Co Academies' => 712,
             'Bergen County Acad' => 712,
             'Brick Twp Mem HS' => 721,
+            'Camden Co Tech School' => 756,
+            'Charter-Tech HS' => 759,
             'Christian Academy' => 728, //Hawthorne Christian Academy
             'Clearview Reg HS' => 95,
+            'Cramford HS' => 115, //typo: Cranford HS
+            'Cranford HS' => 115,
             'Dseneca' => 759, //typo for Seneca HS
             'Dseneca HS' => 759, //typo for Seneca HS
+            'Haddon Twp HS' => 194,
             'Hawthorne Christian Acad' => 728,
             'Hunterdon Cent Reg HS Dist' => 244,
             'John P Stevents HS' => 247, //typo for J P Stevens
             'John P Stevens' => 247,
             'John P. Stevens' => 247,
             'John P. Stevens HS' => 247,
+            'Monroe Twp HS' => 339,
+            'Montville Twp HS' => 342,
+            'N Burlington Co Reg HS' => 375,
             'No Bur Co Reg HS' => 375,
+            'No Highlands Reg HS' => 383,
             'No Jersey Home Sch Assn' => 700,
             'No Valley Reg HS/Old Tappan' => 388,
             'No Valley Reg HS at Old Tappan' => 388,
             'Overbrook Senior HS' => 406,
             'Parsippany Hills HS' => 412,
             'Pascack Valley HS' => 416,
+            'Pemberton HS' => 427,
             'Rutgers Prep School' => 640,
             'W Morris Mendham' => 599,
             'W Morris Mendham HS' => 599,
