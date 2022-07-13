cb0e32969d19638697cad85967dba1f84a541efa
MFRHoldings <rick@mfrholdings.com>
2022-07-13 09:06:28 -0400

    Correct name misspelling and typo.

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,10 +10,10 @@
    \*******************************************************************************************************
    We would like to thank the following for their contributions to this project:
    - Barbara Retzko for her unconditional support and love.
-   - Sidney Volmar for her assistance in the page styling
+   - Sidney Vollmar for her assistance in the page styling
    - Taylor Otwell for his wonderful Laravel
    - Caleb Porzio for his wonderful Livewire and Alpine.js
-   - Povilas Korop for his worderful QuickAdminPanel and ongoing generous contributions to the Laravel community
+   - Povilas Korop for his wonderful QuickAdminPanel and ongoing generous contributions to the Laravel community
    \*******************************************************************************************************
    -->
 
